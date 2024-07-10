fb6fcb530061cc8a86a39b87fe0027dafee1aace
Argi <arkatsy@gmail.com>
2024-07-10 23:57:14 +0300

    metadata

--- windows-theme-toggle.ts
+++ windows-theme-toggle.ts
@@ -1,7 +1,6 @@
 // Name: Toggle System Theme
 // Author: arkatsy
 // Description: Toggles the system theme between light and dark mode.
-// Keywords: windows, theme, dark, light, toggle
 
 import "@johnlindquist/kit";
 import * as _regedit from "regedit";
